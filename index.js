6196fa009c904d7bd4ae36ca70303a80ade9fc15
rob <robertmaticke12@gmail.com>
2022-08-14 12:24:16 -0400

    commit tree c361a79264dabb8c7e5df11f3a841c8eb1614c98

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = 'nancy'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'mely'
+    return leastFavoriteCustomer
+}
